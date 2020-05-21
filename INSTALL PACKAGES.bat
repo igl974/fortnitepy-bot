8ebd9d03b4dc9f69869fdf5ce64c0f4300507194
igl974 <65707544+igl974@users.noreply.github.com>
2020-05-21 10:54:25 +0200

    Vbn
    Csfhb

--- INSTALL PACKAGES.bat
+++ INSTALL PACKAGES.bat
@@ -1,9 +1,1 @@
-@echo off
-ECHO Installing the required packages for the bot!
-TIMEOUT 3
-
-py -3 -m pip install -U -r requirements.txt
-
-ECHO Done! Now run START BOT.bat
-PAUSE
-
+Hi Bots lszoddğjjfdjolkjde37ojxwhjbvv
